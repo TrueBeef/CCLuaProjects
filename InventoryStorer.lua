9ce9b140f14100674a5d344519620d862182ed0f
Sean Carrick <anthonysean0777@gmail.com>
2023-07-04 13:35:21 -0700

    Finished Turtle Inventory Flusher

--- InventoryStorer.lua
+++ InventoryStorer.lua
@@ -1,10 +1,5 @@
-
 local function ListChests()
 	return { peripheral.find("minecraft:chest") }
-end
-
-local function ListTurtles( )
-	return { peripheral.find("turtle") }
 end
 
 local function TurtleToBuffer(turtle, bufferChest)
@@ -15,22 +10,10 @@ local function TurtleToBuffer(turtle, bufferChest)
 	-- if not turtleItem then break end
 	
 	--Loop through turtle inventory
-	for i=1,16, 1 do
-
-		-- make sure this chest has atleast one empty slot.
-		for j=1,bufferChest.size(), 1 do
-
-			local item = bufferChest.getItemDetail(j)
-
-			if(item == nil) then
-				-- Pull from name, from slot, limit, to slot
-				if(peripheral.getName(turtle) ~= nil) then
-					bufferChest.pullItems(peripheral.getName(turtle), i, 64)
-				else 
-					break 
-				end
-
-				break
+	if(turtle ~= nil) then
+		for i=1,16, 1 do			
+			if(turtle ~= nil) then
+				bufferChest.pullItems(peripheral.getName(turtle), i)
 			end
 		end
 	end
@@ -39,49 +22,48 @@ end
 local function BufferToBulk(bufferChest)
 	print("Transferring from buffer to bulk...")	
 	chestTable = ListChests()
-
 	
-		-- turtleItem = turtle.getItemDetail(i)
-		-- if not turtleItem then break end	
-
-	for _, chest in pairs(chestTable) do
-		--Loop through turtle inventory
-		for i=1,16, 1 do
-
-			-- make sure this chest has atleast one empty slot.
-			for j=1,chest.size(), 1 do
-
-				local item = chest.getItemDetail(j)
-
-				if(item == nil) then
-					-- Pull from name, from slot, limit, to slot
-					chest.pullItems(peripheral.getName(peripheral.find(peripheral.getName(bufferChest))), i, 64)
+	for i=1, bufferChest.size(), 1 do
+		for _, chest in pairs(chestTable) do
+			local attempts = 0
+			local bufferItem = bufferChest.getItemDetail(i)
+			while (bufferItem ~= nil) do
+				if(attempts > 2) then
 					break
 				end
+				
+				chest.pullItems(peripheral.getName(bufferChest), i)
+				attempts = attempts + 1
 			end
-		end			
+
+			if(bufferItem == nil) then
+				break
+			end
+		end
 	end
 end
 
 local function WaitForTurtle( )
 	while(true)	do
 		-- Look for turtles
-		turtleTable = ListTurtles()		
-		print("Buffer chest: barrel ")
-		print("Looking for turtle...")
-		for _, turtle in pairs(turtleTable) do
-			-- We have turtles?
-			
-			--Find methods of peripheral. Keep for later tbh.
-			-- turtleMeths = peripheral.getMethods(peripheral.getName(turtle))
-			-- for _, method in pairs(turtleMeths) do
-			--	print(method)
-			--end
-			
-			local bufferChest = peripheral.find("minecraft:barrel")
+		term.clear()
+		term.setCursorPos(1, 1)
+		bufferChest = peripheral.find("minecraft:barrel")
+		BufferToBulk(bufferChest)
+
+		print("- Remember to keep turtle landing modem")
+		print("open with an adjacent inventory of some kind.")
+		print("- Place a barrel for turtle buffer.")
+		print("")
+		print("Waiting for turtle...")
+		
+		local turtle = nil
+		turtle = peripheral.find("turtle")
+
+		if(turtle ~= nil) then
 			TurtleToBuffer(turtle, bufferChest)	
 			BufferToBulk(bufferChest)
-		end
+		end		
 
 		sleep(10)
 	end
