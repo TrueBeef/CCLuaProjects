73e68d57f91f8d1da9c11a4ab3bff47842e7e918
Sean Carrick <anthonysean0777@gmail.com>
2023-07-04 14:28:16 -0700

    Adjusted updater

--- SeanwareInstaller.lua
+++ SeanwareInstaller.lua
@@ -15,7 +15,7 @@ local function InstallFromGit(repoFile, saveTo)
 	if(download) then --checks if download returned true or false
 		local handle = download.readAll() --Reads everything in download
 		download.close() --remember to close the download!
-		local file = fs.open(saveTo,"w") --opens the file defined in 'saveTo' with the permissions to write.
+		local file = fs.open(saveTo .. ".lua","w") --opens the file defined in 'saveTo' with the permissions to write.
 		file.write(handle) --writes all the stuff in handle to the file defined in 'saveTo'
 		file.close() --remember to close the file!
 		term.setTextColor(colors.green)
